9f9d088cbea92b5981b7bcc027903f3137d7d358
danielphandptp <business.edwardp@gmail.com>
2022-07-19 11:02:54 -0400

    implemented the user register functionality and connected it to MongoDB cloud

--- controllers/auth/postRegister.js
+++ controllers/auth/postRegister.js
@@ -1,5 +1,43 @@
+const User = require('../../models/user');
+const bcrypt = require('bcryptjs');
+
 const postRegister = async (req, res) => {
-  res.send('register route');
+  try {
+    // destructuring information from user data
+    const { username, mail, password } = req.body;
+
+    // check and make sure that this is a new user
+    const userExists = await User.exists({ mail: mail.toLowerCase() });
+    if (userExists) {
+      return res.status(409).send('E-mail already in use.');
+    }
+
+    // encrypt the password
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    // create user document and save in database
+    const user = await User.create({
+      username,
+      mail: mail.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    // create JWT token
+    const token = 'JWT TOKEN';
+
+    // send a response to the user
+    res.status(201).json({
+      userDetails: {
+        mail: user.mail,
+        token: token,
+        username: user.username,
+      },
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .send('Error occurred when registering user. Please try again.');
+  }
 };
 
 module.exports = postRegister;
